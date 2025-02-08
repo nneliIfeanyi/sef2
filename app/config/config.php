9469ce7ff844ad4e706765c447f06e54d136eed0
Stanvic <stanvicbest@gmail.com>
2025-02-08 20:59:38 +0100

    deploy init

--- app/config/config.php
+++ app/config/config.php
@@ -2,12 +2,12 @@
 // DB Params
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
-define("DB_PASS", "123456");
-define("DB_NAME", "traversymvc");
+define("DB_PASS", "");
+define("DB_NAME", "sefMVC");
 
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL Root
-define('URLROOT', 'http://localhost/sef');
+define('URLROOT', 'https://sample.stanvic.com.ng');
 // Site Name
 define('SITENAME', 'SEF');
