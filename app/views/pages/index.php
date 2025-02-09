b86a0af640d78eb4c8812208a02592e8e02c38db
Stanvic <stanvicbest@gmail.com>
2025-02-09 15:26:08 +0100

    still on about

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -17,7 +17,7 @@
           </blockquote>
           <div class="d-flex" data-aos="fade-up" data-aos-delay="200">
             <a href="#about" class="btn-get-started">Learn More</a>
-            <a href="https://www.youtube.com/watch?v=Y7f98aduVJ8"
+            <a href="<?php echo URLROOT; ?>/assets/img/new/vid.mp4"
               class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch
                 Video</span></a>
           </div>
